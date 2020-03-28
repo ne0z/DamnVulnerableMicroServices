712004cdb46a3661adb7f9b6500a651e63edf4d7
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 02:37:46 +0800

    Remove unused line

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -15,8 +15,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
