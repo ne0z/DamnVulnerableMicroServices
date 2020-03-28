8918f33b3ff40c61cdaa352ff3e91fd3eb5cfa23
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 02:41:51 +0800

    setup API default path

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use('/api/account', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
