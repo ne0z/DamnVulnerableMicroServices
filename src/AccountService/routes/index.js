70c6c21483c8f6d421416c9ecdfe58cc0ab6dd32
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 03:11:54 +0800

    Define API available methods

--- src/AccountService/routes/index.js
+++ src/AccountService/routes/index.js
@@ -10,4 +10,84 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// Create account
+router.put('/', function(req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    data: {
+      id: 'b065a298-d897-4441-979d-d2f26b0265cf',
+      username: 'danang.heriyadi',
+      password: 'e5828c564f71fea3a12dde8bd5d27063',
+      email: 'danang.ganteng@example.net',
+      full_name: 'Danang Heriyadi',
+      gender: 0
+    }
+  });
+});
+
+// Find account
+router.post('/', function(req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    data: [{
+      id: 'b065a298-d897-4441-979d-d2f26b0265cf',
+      username: 'danang.heriyadi',
+      password: 'e5828c564f71fea3a12dde8bd5d27063',
+      email: 'danang.ganteng@example.net',
+      full_name: 'Danang Heriyadi',
+      gender: 0
+    }]
+  });
+});
+
+// Get account
+router.get('/:user', function(req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    data: {
+      id: 'b065a298-d897-4441-979d-d2f26b0265cf',
+      username: 'danang.heriyadi',
+      password: 'e5828c564f71fea3a12dde8bd5d27063',
+      email: 'danang.ganteng@example.net',
+      full_name: 'Danang Heriyadi',
+      gender: 0
+    }
+  });
+});
+
+// Update account
+router.patch('/:user', function(req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    data: {
+      id: 'b065a298-d897-4441-979d-d2f26b0265cf',
+      username: 'danang.heriyadi',
+      password: 'e5828c564f71fea3a12dde8bd5d27063',
+      email: 'danang.ganteng@example.net',
+      full_name: 'Danang Heriyadi',
+      gender: 0
+    }
+  });
+});
+
+// Delete account
+router.delete('/:user', function(req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    data: {
+      id: 'b065a298-d897-4441-979d-d2f26b0265cf',
+      username: 'danang.heriyadi',
+      password: 'e5828c564f71fea3a12dde8bd5d27063',
+      email: 'danang.ganteng@example.net',
+      full_name: 'Danang Heriyadi',
+      gender: 0
+    }
+  });
+});
+
 module.exports = router;
