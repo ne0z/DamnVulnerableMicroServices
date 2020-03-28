5ac6f793ad6724e6a7a03f65e5af5490d63372cc
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 02:40:20 +0800

    replace error page template

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -31,7 +31,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: 'FAIL',
+    message: err.message
+  });
 });
 
 module.exports = app;
