14044daa6edd125f5f18a501ca0ead9993ec559a
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 04:07:22 +0800

    remove unused required line

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
