a2f8148a2e8bcf6037bef59c361e1705a4b4661f
danang heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 02:41:04 +0800

    remove unused view engine, unused file and folder

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -8,10 +8,6 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
