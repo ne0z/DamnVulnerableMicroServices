18a6db75b30a5f29f03a0d04c8813e546209f547
Danang Heriyadi <danang.heriyadi19@gmail.com>
2020-03-29 16:06:18 +0800

    Account Service API (#4)
    * Remove unused line
    
    * replace error page template
    
    * remove unused view engine, unused file and folder
    
    * setup API default path
    
    * change home response
    
    * Define API available methods
    
    * Initialize OpenAPI documentation for Account Service
    
    * remove unused required line
    
    * API Find account
    
    * API Get account
    
    * API for create, update, delete, search, and get

--- src/AccountService/app.js
+++ src/AccountService/app.js
@@ -1,24 +1,27 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose
+    .connect(db)
+    .then(() => console.log("mongoDB Connected"))
+    .catch((err) => console.log(err));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+app.use('/api/account/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/account', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,7 +36,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: 'FAIL',
+    message: err.message
+  });
 });
 
 module.exports = app;
--- src/AccountService/routes/index.js
+++ src/AccountService/routes/index.js
@@ -1,9 +1,143 @@
 var express = require('express');
 var router = express.Router();
+const Account = require('../models/Account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.status(200);
+  res.json({
+    status: 'OK',
+    app: 'AccountService',
+    version: 'v1.0.0'
+  });
+});
+
+// Create account
+router.put('/', function (req, res, next) {
+  Account.findOne({ email: req.body.email })
+    .then(account => {
+      if (!account) {
+        Account.create({
+          email: req.body.email,
+          username: req.body.username,
+          full_name: req.body.full_name,
+          password: req.body.password
+        }).then( doc => {
+          return res.status(200).json({
+            status: 'OK',
+            data: doc
+          });
+        }).catch(error => {
+          return res.status(400).json({
+            status: 'FAIL',
+            message: error.message
+          })
+        })
+      } else {
+        return res.status(400).json({
+          status: 'FAIL',
+          message: 'Account already exists'
+        })
+      }
+    })
+});
+
+// Find account
+router.post('/', function (req, res, next) {
+  Account.findOne(req.body)
+    .then(account => {
+      if (account) {
+        return res.status(200).json({
+          status: 'OK',
+          data: account
+        });
+      } else {
+        return res.status(400).json({
+          status: 'FAIL',
+          message: 'Account not found'
+        })
+      }
+    })
+    .catch(error => {
+      return res.status(400).json({
+        status: 'FAIL',
+        message: error.message
+      })
+    });
+});
+
+// Get account
+router.get('/:user', function (req, res, next) {
+  var user_id = req.params.user;
+  Account.findOne({
+    _id: user_id
+  })
+    .then(account => {
+      if (account) {
+        return res.status(200).json({
+          status: 'OK',
+          data: account
+        });
+      } else {
+        return res.status(400).json({
+          status: 'FAIL',
+          message: 'Account not found'
+        })
+      }
+    })
+    .catch(error => {
+      return res.status(400).json({
+        status: 'FAIL',
+        message: error.message
+      })
+    });
+});
+
+// Update account
+router.patch('/:user', function (req, res, next) {
+  var user_id = req.params.user;
+  Account.findOneAndUpdate({
+    _id: user_id
+  }, req.body)
+    .then(account => {
+      if (account) {
+        return res.status(200).json({
+          status: 'OK',
+          message: 'Successfully updated'
+        });
+      } else {
+        return res.status(400).json({
+          status: 'FAIL',
+          message: 'Account not found'
+        })
+      }
+    })
+    .catch(error => {
+      return res.status(400).json({
+        status: 'FAIL',
+        message: error.message
+      })
+    });
+});
+
+// Delete account
+router.delete('/:user', function (req, res, next) {
+  var user_id = req.params.user;
+  Account.deleteOne({
+    _id: user_id
+  })
+    .then(account => {
+      return res.status(200).json({
+        status: 'OK',
+        data: account
+      });
+    })
+    .catch(error => {
+      return res.status(400).json({
+        status: 'FAIL',
+        data: error.message
+      })
+    })
+    ;
 });
 
 module.exports = router;
